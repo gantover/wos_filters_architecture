a51b1a945780fc0ad04ae388032a7fc5605e700a
Damian Hubert <gantoverpro@gmail.com>
2025-04-01 16:38:13 +0200

    Stack filter experiment

---
+++ src/main/scala/wos/stack_filters/stack_filters.scala
@@ -0,0 +1,108 @@
+package wos.stack_filters
+
+import chisel3._
+import chisel3.util._
+import chisel3.util.experimental.loadMemoryFromFileInline
+
+class ThresholdDecomposition(width: Int) extends Module {
+    val out_dim = scala.math.pow(2,width).toInt - 1
+  val io = IO(new Bundle {
+    val x = Input(UInt(width.W))
+    val out = Output(UInt(out_dim.W))
+  })
+  io.out := VecInit(Seq.tabulate(out_dim)(i => ((i+1).U <= io.x).asUInt)).asUInt
+}
+
+class ShiftRegister(val width: Int, val depth: Int) extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(width.W))
+    val out = Output(UInt((depth * width).W))
+    val transfer = Output(UInt(width.W))
+  })
+  val regs = Seq.fill(depth)(RegInit(0.U(width.W)))
+  regs.head := io.in
+  io.transfer := regs.head
+  for (i <- 1 until depth) {
+    regs(i) := regs(i-1)
+  }
+  io.out := Cat(regs) 
+}
+
+class Regs(val depth: Int) extends Module {
+    assert(depth > 1)
+    val io = IO(new Bundle {
+        val in = Input(UInt(1.W))
+        val out = Output(UInt(depth.W))
+    })
+
+    // val regs = RegInit(VecInit(Seq.fill(depth)(0.U(1.W))))
+    val regs = RegInit(0.U(depth.W))
+    // regs := regs << 1.U ## io.in
+    // regs := Cat(regs(depth-2, 0), io.in)
+    regs := Cat(io.in, regs(depth-1, 1)) // ->in$$-> MSB contains the new value
+    io.out := regs
+}
+
+class BLL(val weights : Array[Int], val rank : Int, val width : Int) extends Module {
+    val depth = weights.length
+    val io = IO(new Bundle {
+        val regs_in = Input(UInt(depth.W))
+        val out = Output(UInt(1.W))
+    })
+    val acc = Wire(UInt(width.W))
+    acc := 0.U
+    // for (i <- 0 until depth) { // until does not include the last element
+    //     acc := acc + Mux(io.regs_in(i) === 1.U, weights(i).U(width.W), 0.U(width.W))
+    // }
+    acc := weights.zipWithIndex.foldLeft(0.U(width.W)) { case (sum, (weight, i)) =>
+        sum + Mux(io.regs_in(i) === 1.U, weight.U(width.W), 0.U(width.W))
+    }
+    io.out := (acc >= rank.U).asUInt
+}
+
+class ThresholdRecomposition(width: Int) extends Module {
+    val in_dim = scala.math.pow(2, width).toInt - 1
+    val io = IO(new Bundle {
+        val in = Input(UInt(in_dim.W))
+        val out = Output(UInt(width.W))
+    })
+    when(io.in(in_dim-1) === 1.U) {
+        io.out := in_dim.U
+    } .otherwise {
+        io.out := PriorityEncoder(~io.in)
+    }
+}
+
+class StackFiltersUnit(val weights : Array[Int], val rank : Int, val width : Int) extends Module {
+    // val mem_file = "./src/main/resources/stack_filters/stack_filters.mem"
+    val depth = weights.length
+    val exp_dim = scala.math.pow(2, width).toInt - 1
+    val io = IO(new Bundle {
+        val x = Input(UInt(width.W))
+        val y = Output(UInt(width.W))
+    })
+
+    // val mem = SyncReadMem(8, UInt(width.W))
+    // assert (mem_file.trim().nonEmpty)
+    // loadMemoryFromFileInline(mem, mem_file)
+
+    val tdu = Module(new ThresholdDecomposition(width))
+    val regs_array = Array.fill(exp_dim)(Module(new Regs(depth)))
+    val bll_array = Array.fill(exp_dim)(Module(new BLL(weights, rank, width)))
+    val tru = Module(new ThresholdRecomposition(width))
+    tdu.io.x := io.x
+    for (i <- 0 until exp_dim) {
+        regs_array(i).io.in := tdu.io.out(i)
+        bll_array(i).io.regs_in := regs_array(i).io.out
+    }
+    val bll_outputs = Cat(bll_array.map(_.io.out).reverse)
+    for (i <- 0 until exp_dim) {
+        printf(p"regs_array($i).io.in: ${regs_array(i).io.in}\n")
+        printf(p"regs_array($i).io.out: ${regs_array(i).io.out}\n")
+        printf(p"bll_array($i).io.regs_in: ${bll_array(i).io.regs_in}\n")
+        printf(p"bll_array($i).io.out: ${bll_array(i).io.out}\n")
+    }
+    // printf("bll_outputs: %b\n", bll_outputs)
+    tru.io.in := bll_outputs
+    io.y := tru.io.out
+}
---
+++ src/test/scala/wos/stack_filters/tb_stack_filters.scala
@@ -0,0 +1,89 @@
+package wos.stack_filters
+
+import org.scalatest.flatspec.AnyFlatSpec
+import chisel3._
+import chiseltest._
+
+class tb_stack_filters extends AnyFlatSpec with ChiselScalatestTester {
+    "ThresholdDecomposition" should "pass" in {
+        test(new ThresholdDecomposition(3)) { c =>
+            c.io.x.poke(0.U)
+            c.io.out.expect(0.U)
+            c.io.x.poke(6.U)
+            c.io.out.expect("b0111111".U)
+            c.io.x.poke(7.U)
+            c.io.out.expect("b1111111".U)
+        }
+    }
+    // "ShiftReg" should "pass" in {
+    //     test(new ShiftRegister(8, 3)) { c => 
+    //         c.io.in.poke("b11111111".U)
+    //         c.clock.step()
+    //         c.io.transfer.expect("b11111111".U)
+    //         c.io.in.poke("b11111111".U)
+    //         c.clock.step()
+    //         c.io.in.poke("b11111111".U)
+    //         // the input of reg 2 to 3 is ready but we need a last clock cycle to transfer the value
+    //         c.clock.step()
+    //         c.io.out.expect("b111111111111111111111111".U)
+    //     }
+    // }
+    "Regs" should "pass" in {
+        test(new Regs(3)) { c => 
+            c.io.in.poke(1.U)
+            c.clock.step()
+            c.io.in.poke(1.U)
+            c.clock.step()
+            c.io.out.expect("b110".U)
+            c.io.in.poke(1.U)
+            c.clock.step()
+            c.io.out.expect("b111".U)
+        }
+    }
+    "BLL" should "pass" in {
+        test(new BLL(Array(1, 2, 3), 4, 4)) { c => 
+            // 1,2,3 with the 4th rank, and bit width of 4 for the weights
+            c.io.regs_in.poke("b110".U)
+            // 101 means 3 + 2 + 0 = 5 so we should have 1 at the output
+            c.io.out.expect(1.U)
+            c.io.regs_in.poke("b011".U)
+            // 101 means 0 + 2 + 1 = 3 so we should have 0 at the output
+            c.io.out.expect(0.U)
+        }
+    }
+    "TR" should "pass" in {
+        test(new ThresholdRecomposition(3)) { c =>
+            c.io.in.poke("b1111111".U)
+            c.io.out.expect(7.U)
+            c.io.in.poke("b0000000".U)
+            c.io.out.expect(0.U)
+            c.io.in.poke("b0000001".U)
+            c.io.out.expect(1.U)
+            c.io.in.poke("b0000011".U)
+            c.io.out.expect(2.U)
+            c.io.in.poke("b0111111".U)
+            c.io.out.expect(6.U)
+        }
+    }
+    "StackFiltersUnit" should "pass" in {
+        test(new StackFiltersUnit(Array(1, 2, 3), 4, 3)) { c =>
+            c.io.x.poke(1.U)
+            c.clock.step()
+            c.io.x.poke(3.U)
+            c.clock.step()
+            c.io.x.poke(2.U)
+            c.clock.step()
+            println("StackFiltersUnit Output " + c.io.y.peek().litValue)
+            c.io.x.poke(3.U)
+            c.clock.step()
+            println("StackFiltersUnit Output " + c.io.y.peek().litValue)
+            c.io.x.poke(5.U)
+            c.clock.step()
+            println("StackFiltersUnit Output " + c.io.y.peek().litValue)
+            c.io.x.poke(5.U)
+            c.clock.step()
+            println("StackFiltersUnit Output " + c.io.y.peek().litValue)
+        }
+    }
+
+}
